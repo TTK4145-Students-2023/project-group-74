674d9fff5ad45180b51d6dea4dbafd72c720264b
erlendskinnemoen <75087816+erlendskinnemoen@users.noreply.github.com>
2023-03-20 18:50:16 +0100

    Update localTypes.go

--- localTypes/localTypes.go
+++ localTypes/localTypes.go
@@ -1,9 +1,12 @@
 package localTypes
 
-import 
+import (
   //project config 
   "strconv"
-
+  "time"
+  "net"
+  "project-group-74/network"
+)
 // ----- CONSTANTS ------ // 
 // Create an init file with the following constants
 // Time
@@ -16,7 +19,7 @@ const (
   
   OPEN_DOOR_TIME_sek            = 3
   TRAVEL_TIME_sek               = 3
-  MAX_TIME_TO_FINISH_ORDER      = 3*(NUM_FLOORS-1)*(TRAVEL_TIME*OPEN_DOOR_TIME)
+  MAX_TIME_TO_FINISH_ORDER      = 3*(NUM_FLOORS-1)*(TRAVEL_TIME_sek*OPEN_DOOR_TIME_sek)
 )
 
 // ----- TYPE DEFINITIONS ------ // 
@@ -27,7 +30,7 @@ type BUTTON_TYPE int
 const(
   Button_Cab      BUTTON_TYPE = 0
   Button_hall_up              = 1
-  Buton_hall_down            = 2
+  Button_hall_down            = 2
 )
 
 type BUTTON_INFO struct{
@@ -35,8 +38,8 @@ type BUTTON_INFO struct{
   Button  BUTTON_TYPE
 }
 
-type HMATRIX [NUM_FLOORS][types.NUM_BUTTONS-1]bool
-type ORDER map[string][types.NUM_FLOORS][types.NUM_BUTTONS-1]bool
+type HMATRIX [NUM_FLOORS][NUM_BUTTONS-1]bool
+type ORDER map[string][NUM_FLOORS][NUM_BUTTONS-1]bool
 type P2P_ELEV_INFO []LOCAL_ELEVATOR_INFO
 
 
@@ -68,17 +71,17 @@ const(
   DIR_up              =  1
 )
 
-const ORDER_WATCHDOG_POLL_RATE = config.ORDER_WATCHDOG_POLL_RATE_MS * time.Millisecond
+const ORDER_WATCHDOG_POLL_RATE = 50 * time.Millisecond
 
 type HRAElevState struct {
 	State       string                      `json:"behaviour"`
 	Floor       int                         `json:"floor"`
 	Direction   string                      `json:"direction"`
-	CabRequests [localTypes.NUM_FLOORS]bool `json:"cabRequests"`
+	CabRequests [NUM_FLOORS]bool `json:"cabRequests"`
 }
 
 type HRAInput struct {
-	HallRequests [localTypes.NUM_FLOORS][2]bool `json:"hallRequests"`
+	HallRequests [NUM_FLOORS][2]bool `json:"hallRequests"`
 	States       map[string]HRAElevState        `json:"states"`
 }
 
@@ -94,7 +97,7 @@ type OAInputs struct {
 	ordersFromNetwork <-chan HRAInput
 	ordersFromMaster  <-chan []byte
 	ordersToSlave     chan<- []byte
-	localOrder        chan<- [localTypes.NUM_FLOORS][2]bool
+	localOrder        chan<- [NUM_FLOORS][2]bool
 }
 
 
@@ -128,21 +131,20 @@ func (btnInfo BUTTON_INFO) isValid() bool{
   return btnInfo.Button.isValid() && isValidFloor(btnInfo.Floor)
 }
 
-func (order ORDER_TYPE) isValid() bool{
-  return BUTTON_INFO(order).isValid() 
+func (order FOREIGN_ORDER_TYPE) isValid() bool{
+  return BUTTON_INFO(order.Foreign_order).isValid() 
 }
 
 func (dir MOTOR_DIR) isValid() bool{
-  return dir == Dir_down  ||
-         dir == Dir_up    ||
-         dir == Dir_stop  
+  return dir == DIR_down  ||
+         dir == DIR_up    ||
+         dir == DIR_stop  
 }
 
 func (loc_elev LOCAL_ELEVATOR_INFO) isValid() bool{
-  return isValidFloor(loc_elev.floor) &&
+  return isValidFloor(loc_elev.Floor) &&
          loc_elev.Direction.isValid() &&
-         loc_elev.State.isValid()     &&
-  
+         loc_elev.State.isValid()     
 }
 
 // ----- FUNCTIONS (GET/SET) ------ // 
@@ -153,7 +155,7 @@ func splitIPAddr (ip string)byte{
 
 func CompareIPAddr (MyIP string, Peers []string)bool{
 	lowestIP := Peers[0]
-	for _, ip := range peers.Peers[1:]{
+	for _, ip := range network.PeerList[1:]{
 		lastOctet := splitIPAddr(ip)
 		addrLowest := net.ParseIP(lowestIP).To4()
 		if lastOctet < addrLowest[3]{
