8ec86cd455b34830fc3ebd3c453d99f2b0910e70
Pavel <pavel.skipenes@ntnu.no>
2023-04-13 22:37:21 +0200

    OA CHANGES??

--- decision/orderAssigner.go
+++ decision/orderAssigner.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"project-group-74/decision/DLOCC"
 	"project-group-74/localTypes"
+	"reflect"
 	"runtime"
 	"time"
 )
@@ -28,6 +29,8 @@ func OrderAssigner(
 	}
 
 	currentHRAInput := DLOCC.NewAllFalseHRAInput()
+	lastOrders := DLOCC.ReassignOrders(currentHRAInput, hraExecutable)
+	var OAticker = time.NewTicker(time.Millisecond * 999)
 
 	for {
 		select {
@@ -38,18 +41,6 @@ func OrderAssigner(
 			newHRAelev := DLOCC.LocalState2HRASTATE(newElevInfo)
 			currentHRAInput.States[newElevInfo.ElevID] = newHRAelev
 
-			if localTypes.IsMaster(localTypes.MyIP, localTypes.PeerList.Peers) {
-				newOrders := DLOCC.ReassignOrders(currentHRAInput, hraExecutable)
-				/*for k, v := range newOrders {
-					fmt.Printf("New Orders: %s: %v\n", k, v)
-				}*/
-				if len(localTypes.PeerList.Peers) == 0 {
-					RxNewOrdersChan <- newOrders
-				} else {
-					TxNewOrdersChan <- newOrders
-				}
-			}
-
 		case newHRequest := <-RxNewHallRequestChan:
 			//if !isValidFloor(newHRequest.Floor) || newHRequest.Button !isValid(){
 			//	panic("Corrupt elevator data from RxNewHallRequestChan")
@@ -57,19 +48,6 @@ func OrderAssigner(
 			if !currentHRAInput.HallRequests[newHRequest.Floor][newHRequest.Button] {
 				currentHRAInput.HallRequests[newHRequest.Floor][newHRequest.Button] = true
 				fmt.Printf("DLOCC: NewHrequest: \n")
-
-				if localTypes.IsMaster(localTypes.MyIP, localTypes.PeerList.Peers) {
-					newOrders := DLOCC.ReassignOrders(currentHRAInput, hraExecutable)
-					for k, v := range newOrders {
-						fmt.Printf("New Orders: %s: %v\n", k, v)
-					}
-					if len(localTypes.PeerList.Peers) == 0 {
-						RxNewOrdersChan <- newOrders
-					} else {
-						TxNewOrdersChan <- newOrders
-					}
-				}
-
 			}
 
 		case finishedHOrder := <-RxFinishedHallOrderChan:
@@ -79,18 +57,23 @@ func OrderAssigner(
 			fmt.Printf("DLOCC: new finishedHOrder: %v\n \n", currentHRAInput)
 			currentHRAInput.HallRequests[finishedHOrder.Floor][finishedHOrder.Button] = false
 
+		case <-OAticker.C:
 			if localTypes.IsMaster(localTypes.MyIP, localTypes.PeerList.Peers) {
+
 				newOrders := DLOCC.ReassignOrders(currentHRAInput, hraExecutable)
-				for k, v := range newOrders {
-					fmt.Printf("New Orders: %s: %v\n", k, v)
-				}
-				if len(localTypes.PeerList.Peers) == 0 {
-					RxNewOrdersChan <- newOrders
-				} else {
-					TxNewOrdersChan <- newOrders
+				if !reflect.DeepEqual(newOrders, lastOrders) {
+					lastOrders = newOrders
+
+					if len(localTypes.PeerList.Peers) == 0 {
+						RxNewOrdersChan <- newOrders
+					} else {
+						TxNewOrdersChan <- newOrders
+					}
+					for k, v := range newOrders {
+						fmt.Printf("New Orders from ticker: %s: %v\n", k, v)
+					}
 				}
 			}
-
 		default:
 			time.Sleep((time.Millisecond * 200))
 		}
